4840f6b4ca762bc2631a3f5137cdf6168f83a628
Pradeep Chodisetti <pradeepchodisetti@Pradeeps-MacBook-Pro.local>
2018-06-10 17:43:00 +0530

    Update config to use json file

--- server/tests/seed/seed.js
+++ server/tests/seed/seed.js
@@ -15,7 +15,7 @@ const users = [{
 		token: jwt.sign({ 
 			_id: userOneId,
 			access: 'auth'
-		}, 'abc123').toString() 
+		}, process.env.JWT_SECRET).toString() 
 	}]
 }, {
 	_id: userTwoId,
@@ -26,7 +26,7 @@ const users = [{
 		token: jwt.sign({ 
 			_id: userTwoId,
 			access: 'auth'
-		}, 'abc123').toString() 
+		}, process.env.JWT_SECRET).toString() 
 	}]
 }];
 
